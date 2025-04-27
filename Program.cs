26698a12547b02cc30703e784f5cb39110ed5bda
Miguel araujo grosz <miguelgrosz12@gmail.com>
2025-04-27 17:11:21 -0300

    3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿ Console.WriteLine("Digite o primeiro número:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite o terceiro número:");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine("OMaior e o num1!");
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine("OMaior e o num2!");
+}
+else if (num3 > num1 && num3 > num2)
+{
+    Console.WriteLine("OMaior e o num3!");
+}
+else
+{
+    Console.WriteLine("A dois numero Grandes.");
+}
+
+    
